622bcb8b176354d0495c8a933c9b8005b1ce8d2f
EM4360 <emapatimomo13@gmail.com>
2023-08-22 11:25:35 -0300

    Agregamos css al formulario de informacion de la mascota

--- src/app/modules/form-publicar/formpublicar.module.ts
+++ src/app/modules/form-publicar/formpublicar.module.ts
@@ -5,13 +5,15 @@ import { FormpublicarRoutingModule } from './formpublicar-routing.module';
 import { InfomascotaComponent } from './Components/infomascota/infomascota.component';
 import { UbicacionComponent } from './Components/ubicacion/ubicacion.component';
 import { ContactoComponent } from './Components/contacto/contacto.component';
+import { PaginapublicarComponent } from './pages/paginapublicar/paginapublicar.component';
 
 
 @NgModule({
   declarations: [
     InfomascotaComponent,
     UbicacionComponent,
-    ContactoComponent
+    ContactoComponent,
+    PaginapublicarComponent
   ],
   imports: [
     CommonModule,
