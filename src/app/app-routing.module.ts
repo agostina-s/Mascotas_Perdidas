342b443a67eb7a12a40a4b4ffa57dc64c31514b6
agostina-s <agosaucedo17@gmail.com>
2023-09-05 10:32:22 -0300

    Creacion del primer card info-mascotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ const routes: Routes = [
   {path: 'home', loadChildren:()=>import('./modules/home/home.module').then( m => m.HomeModule)},
   {path: 'explore', loadChildren:()=>import('./modules/explore/explore.module').then( m => m.ExploreModule)},
   {path: 'auth', loadChildren:()=>import('./modules/auth/auth.module').then( m => m.AuthModule)}
+  
 
 ];
 
