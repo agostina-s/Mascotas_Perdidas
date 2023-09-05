047ac59e1ed75a5a9eed140cabbf431f70930ddf
agostina-s <agosaucedo17@gmail.com>
2023-09-05 10:15:15 -0300

    Creacion del modulo formulario encontre

---
+++ src/app/modules/form-encontre/form-encontre.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FormEncontreRoutingModule } from './form-encontre-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FormEncontreRoutingModule
+  ]
+})
+export class FormEncontreModule { }
