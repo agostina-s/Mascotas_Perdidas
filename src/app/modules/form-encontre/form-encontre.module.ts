59435a3543dd8149ddeeb97c59bc320ee5f1842d
agostina-s <agosaucedo17@gmail.com>
2023-09-05 10:19:30 -0300

    Creacion de componente ubicacion

--- src/app/modules/form-encontre/form-encontre.module.ts
+++ src/app/modules/form-encontre/form-encontre.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { FormEncontreRoutingModule } from './form-encontre-routing.module';
 import { InfoMascotaComponent } from './components/info-mascota/info-mascota.component';
+import { UbicacionComponent } from './components/ubicacion/ubicacion.component';
 
 
 @NgModule({
   declarations: [
-    InfoMascotaComponent
+    InfoMascotaComponent,
+    UbicacionComponent
   ],
   imports: [
     CommonModule,
