3b3c1da45fe7aa1e4708b145ea0859a0016c21de
Gabituts <gabrielbastias25@gmail.com>
2023-09-12 00:09:17 -0300

    funcion para loguearse

--- src/app/modules/auth/services/auth.service.ts
+++ src/app/modules/auth/services/auth.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
 
+//servicio de autentificacion de firebase
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  //referenciamos auth de firebase
+  constructor(public auth: AngularFireAuth) { }
+
+
+  //funcion para REGISTRARSE
+  registrar(email:string, password: string){
+    //retorna el nuevo valor de mail y contraseña
+    return this.auth.createUserWithEmailAndPassword(email, password);
+  }
+
+  //funcion para LOGEARSE
+  iniciarSesion(email: string, contrasena: string){
+    // valida email y contraseña de la BD
+    return this.auth.signInWithEmailAndPassword(email, contrasena)
+  }
+
+
+  //funcion para TOMAR UID
+  async getuid(){
+    //genera una promesa y user la captura 
+    const user = await this.auth.currentUser;
+
+    if(user == null){
+      return null;
+    }else{
+      return user.uid;
+    }
+  }
 }
