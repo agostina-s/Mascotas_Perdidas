e6d6e6c02dc646b40a46c21ce6ac98e67fed1794
Gabituts <gabrielbastias25@gmail.com>
2023-09-12 00:25:35 -0300

    clase register component, referencia a interfaz usuario, uid y coleccionUsuarios

--- src/app/modules/auth/pages/register/register.component.ts
+++ src/app/modules/auth/pages/register/register.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 //importaciones
 import { AuthService } from '../../services/auth.service';
 import { Usuario } from 'src/app/models/usuario';
-//import { FirestoreService } from 'src/app/shared/services/firestore.service';
+import { FirestoreService } from 'src/app/shared/services/firestore.service';
 import { Router } from '@angular/router';
 
 
@@ -12,5 +12,17 @@ import { Router } from '@angular/router';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  hide = true; // esto es del input
 
+  usuarios: Usuario = {
+    uid: '',
+    name: '',
+    email: '',
+    password: ''
+  }
+
+  uid = '';
+
+  // creamos una nueva colección para usuarios
+  coleccionUsuarios: Usuario[] = [];
 }
