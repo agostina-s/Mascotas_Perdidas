46289531e3ee205a58ae4be0c217a1ff44837056
Gabituts <gabrielbastias25@gmail.com>
2023-09-12 01:31:40 -0300

    cambio en enviroment.ts credenciales actualizadas, ya no es prueba

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -2,12 +2,13 @@ export const enviroment = {
     production: false,
     //copy paste firebase cambiando = por : y sin ;
     firebaseConfig: {
-        apiKey: "AIzaSyCmkgu8XaQm5SUhUrXtsTAigQ_O_KbFX1s",
-        authDomain: "prueba-0g.firebaseapp.com",
-        projectId: "prueba-0g",
-        storageBucket: "prueba-0g.appspot.com",
-        messagingSenderId: "185499322790",
-        appId: "1:185499322790:web:55c5584f5d0e93b9dca767"
+        apiKey: "AIzaSyBm6ORePlcINZFVHkOUNKaRva_2LBIuO-U",
+        authDomain: "mascotasperdidas-56462.firebaseapp.com",
+        projectId: "mascotasperdidas-56462",
+        storageBucket: "mascotasperdidas-56462.appspot.com",
+        messagingSenderId: "605533100749",
+        appId: "1:605533100749:web:e735d394bd636f34592784",
+        measurementId: "G-TEJZCC6X12"
     }
 }
 
