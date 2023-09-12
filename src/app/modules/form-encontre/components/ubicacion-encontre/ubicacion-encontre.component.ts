53ebac82fed0f1d9b19d3834793e7379efb9fcbd
agostina-s <agosaucedo17@gmail.com>
2023-09-12 09:14:34 -0300

    Creamos el componente de ubicacion

---
+++ src/app/modules/form-encontre/components/ubicacion-encontre/ubicacion-encontre.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ubicacion-encontre',
+  templateUrl: './ubicacion-encontre.component.html',
+  styleUrls: ['./ubicacion-encontre.component.css']
+})
+export class UbicacionEncontreComponent {
+
+}
--- src/app/modules/form-encontre/form-encontre.module.ts
+++ src/app/modules/form-encontre/form-encontre.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { FormEncontreRoutingModule } from './form-encontre-routing.module';
 import { ContactoEncontreComponent } from './components/contacto-encontre/contacto-encontre.component';
+import { UbicacionEncontreComponent } from './components/ubicacion-encontre/ubicacion-encontre.component';
 
 
 @NgModule({
   declarations: [
-    ContactoEncontreComponent
+    ContactoEncontreComponent,
+    UbicacionEncontreComponent
   ],
   imports: [
     CommonModule,
