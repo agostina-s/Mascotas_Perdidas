585681e3e0feaeb4aad229ab046ae7d2e6d36bd6
EM4360 <emapatimomo13@gmail.com>
2023-10-04 09:34:28 -0300

    creamos el servicio dentro de admin

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { TablaComponent } from './components/tabla/tabla.component';
+import { AdminComponent } from './pages/admin/admin.component';
 
 
 @NgModule({
   declarations: [
-    TablaComponent
+    TablaComponent,
+    AdminComponent
   ],
   imports: [
     CommonModule,
