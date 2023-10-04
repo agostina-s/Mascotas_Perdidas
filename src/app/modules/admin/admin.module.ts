f07a37551160dcd93b75baaddd1d3eb0edb8b9a9
EM4360 <emapatimomo13@gmail.com>
2023-10-04 09:30:06 -0300

    creamos el apartado componente y el  componente tabla dentro de admin

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
+import { TablaComponent } from './components/tabla/tabla.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TablaComponent
+  ],
   imports: [
     CommonModule,
     AdminRoutingModule
