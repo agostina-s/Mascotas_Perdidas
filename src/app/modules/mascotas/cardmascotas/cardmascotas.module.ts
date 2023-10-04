395ab168267c5d9dec275b624cda3c80fda34316
EM4360 <emapatimomo13@gmail.com>
2023-10-04 10:22:50 -0300

    creamos el modules mascotas

---
+++ src/app/modules/mascotas/cardmascotas/cardmascotas.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CardmascotasRoutingModule } from './cardmascotas-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CardmascotasRoutingModule
+  ]
+})
+export class CardmascotasModule { }
