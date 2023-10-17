b10476b6326bfed4db8ce223bf1ea74f4df9a330
EM4360 <emapatimomo13@gmail.com>
2023-10-17 09:18:02 -0300

    creamos los parametros en el interfaz mascotasencontradas

--- src/app/models/mascotasencontrada.ts
+++ src/app/models/mascotasencontrada.ts
@@ -1,2 +1,13 @@
 export interface Mascotasencontrada {
+    uid:string;
+    raza:string;
+    tamaño:string;
+    collar:string;
+    nombre:string;
+    sexo:string;
+    descripcion:string;
+    imagenprincipal:string;
+    imagenn2:string;
+    imagenn3:string;
+    imagenn4:string;
 }
