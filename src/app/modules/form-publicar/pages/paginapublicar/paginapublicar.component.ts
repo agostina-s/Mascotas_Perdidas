8b14914714504fdb9438c92d31896474d9c17450
EM4360 <emapatimomo13@gmail.com>
2023-10-17 10:58:40 -0300

    creamos los valores para el formulario

--- src/app/modules/form-publicar/pages/paginapublicar/paginapublicar.component.ts
+++ src/app/modules/form-publicar/pages/paginapublicar/paginapublicar.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
-
+import { FormControl, FormGroup, Validators} from '@angular/forms';
+import { Mascotas } from 'src/app/models/mascotasperdidas';
+import { ServicesService } from '../../../admin/services/services.service';
 @Component({
   selector: 'app-paginapublicar',
   templateUrl: './paginapublicar.component.html',
   styleUrls: ['./paginapublicar.component.css']
 })
 export class PaginapublicarComponent {
+  coleccionmascotas: Mascotas [] = [];
+  productoSeleccionado!: Mascotas; // ! -> toma valores vacios
+
+  mascotas = new FormGroup({
+    raza: new FormControl("",Validators.required),
+    tamano: new FormControl("",Validators.required),
+    edad: new FormControl(0,Validators.required ),
+    nombre: new FormControl("",Validators.required),
+    sexo:new FormControl("",Validators.required),
+    descripcion: new FormControl("",Validators.required),
+    perdida: new FormControl("",Validators.required),
+    imagenprincipal: new FormControl("",Validators.required),
+    imagenn2: new FormControl("",Validators.required),
+    imagenn3: new FormControl("",Validators.required),
+    imagenn4: new FormControl("",Validators.required),
+  })
+  
+  constructor(
+    public servicioCrud: ServicesService
+  ){}
+
+  ngOnInit():void{
+    this.servicioCrud.obtenerMascota().subscribe(mascotas =>{
+      this.coleccionmascotas = mascotas
+    })
+  }
+
+  async agregarmascotas(){
+    if
+  }
+
 
 }
+
+
