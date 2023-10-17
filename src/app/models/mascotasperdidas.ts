bbc959a9c1c88d4d5f91a32f2424a63adb68936b
EM4360 <emapatimomo13@gmail.com>
2023-10-17 09:06:33 -0300

    creamos el interfaz contacto y los parametros en el interfaz mascotas perdidas

--- src/app/models/mascotasperdidas.ts
+++ src/app/models/mascotasperdidas.ts
@@ -1,3 +1,14 @@
 export interface Mascotas {
-    
+    uid: string;
+    raza: string;
+    tamaño: string;
+    edad: number;
+    nombre:string;
+    sexo:string;
+    descripcion:string;
+    perdida:string;
+    imagenprincipal:string;
+    imagenn2:string;
+    imagen3:string;
+    imagen4:string;
 }
