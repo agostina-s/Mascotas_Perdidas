17014944c6c887eef7c452aca2a51f69926fd159
Gabituts <gabrielbastias25@gmail.com>
2023-10-21 13:59:27 -0300

    llamando servicio CRUD en card.ts con el metodo GET para mostrar mascotas y uso de la directiva ngfor

--- src/app/modules/explore/componentes/card/card.component.ts
+++ src/app/modules/explore/componentes/card/card.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Mascotas } from 'src/app/models/mascotasperdidas';
+//crud service
+import { ServicesService } from 'src/app/modules/admin/services/services.service';
 
 @Component({
   selector: 'app-card',
@@ -6,5 +9,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent {
+  coleccionMascotas: Mascotas[] = [];
 
+  publiSeleccionada!: Mascotas;
+
+  constructor(
+    //declaramos el SERVICIO CRUD
+    public servicioCRUD: ServicesService
+  ){}
+
+  ngOnInit(): void{
+    /* del servicio Crud, llamamos a obtener producto y los guardamos
+    en la colección */
+    this.servicioCRUD.obtenerMascota().subscribe(mascota =>{
+      this.coleccionMascotas = mascota;
+    })
+  }
+  masInfo(info: Mascotas){
+        // info resguarda la información del producto seleccionado
+        this.publiSeleccionada = info;
+      
+  }
 }
