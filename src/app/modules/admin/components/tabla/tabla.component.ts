fdae8bdad111d35ad4c6e86e669c0cd4acdbc750
EM4360 <emapatimomo13@gmail.com>
2023-11-07 10:18:42 -0300

    en la tabla.ts creamos la coleccion y llamamos al servicio obtener mascotas

--- src/app/modules/admin/components/tabla/tabla.component.ts
+++ src/app/modules/admin/components/tabla/tabla.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { Mascotas } from 'src/app/models/mascotasperdidas';
+import { ServicesService } from 'src/app/modules/admin/services/services.service';
 
 @Component({
   selector: 'app-tabla',
@@ -7,7 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./tabla.component.css']
 })
 export class TablaComponent {
+  coleccionMascotas: Mascotas[] = [];
 
+  publiSeleccionada!: Mascotas;
+
+  constructor(
+    //declaramos el SERVICIO CRUD
+    public servicioCRUD: ServicesService
+  ){}
+
+  ngOnInit(): void{
+    /* del servicio Crud, llamamos a obtener producto y los guardamos
+    en la colección */
+    this.servicioCRUD.obtenerMascota().subscribe(mascota =>{
+      this.coleccionMascotas = mascota;
+    })
+  }
 
 
 }
