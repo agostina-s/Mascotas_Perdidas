6d52109bdf39fd7ac72740734a80416e90eff0d7
Gabituts <gabrielbastias25@gmail.com>
2023-11-07 21:51:35 -0300

    llamando al servicio y imagenes en el html

--- src/app/modules/home/pages/inicio/inicio.component.ts
+++ src/app/modules/home/pages/inicio/inicio.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Mascotas } from 'src/app/models/mascotasperdidas';
+import { ServicesService } from 'src/app/modules/admin/services/services.service';
 import * as $ from 'jquery';
 
 @Component({
@@ -8,7 +10,25 @@ import * as $ from 'jquery';
 })
 export class InicioComponent {
 
+  coleccionMascotas: Mascotas[] = [];
+
+  publiSeleccionada!: Mascotas;
+
+  constructor(
+    //declaramos el SERVICIO CRUD
+    public servicioCRUD: ServicesService
+  ){}
+
+  ngOnInit(): void{
+    /* del servicio Crud, llamamos a obtener producto y los guardamos
+    en la colección */
+    this.servicioCRUD.obtenerMascota().subscribe(mascota =>{
+      this.coleccionMascotas = mascota;
+    })
+  }
+
+
   // CARROUSEL
 
-  
+
 }
