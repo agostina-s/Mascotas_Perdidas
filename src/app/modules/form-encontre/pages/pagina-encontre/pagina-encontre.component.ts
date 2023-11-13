e2c924354b2015938e2d3d28dc4734a582a3220c
SCanterA <salvador.cantera13@gmail.com>
2023-11-13 10:30:00 -0300

    agregue placeholder

--- src/app/modules/form-encontre/pages/pagina-encontre/pagina-encontre.component.ts
+++ src/app/modules/form-encontre/pages/pagina-encontre/pagina-encontre.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+import { FormControl, FormGroup, Validators} from '@angular/forms';
+import { Mascotas } from 'src/app/models/mascotasperdidas';
+import { ServicesService } from '../../../admin/services/services.service';
 @Component({
   selector: 'app-pagina-encontre',
   templateUrl: './pagina-encontre.component.html',
