0d3738c5b05ee0b222a2266f2632bc723e4cd946
EM4360 <emapatimomo13@gmail.com>
2023-11-27 10:12:21 -0300

    le damos las propiedades al objeto mascotas para, luego llenamos el route y mascota service en el constructor

--- src/app/modules/editarmascota/editarmascota.component.ts
+++ src/app/modules/editarmascota/editarmascota.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Mascotas } from 'src/app/models/mascotasperdidas';
+import { ServicesService } from '../admin/services/services.service';
 
 @Component({
   selector: 'app-editarmascota',
@@ -6,5 +9,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./editarmascota.component.css']
 })
 export class EditarmascotaComponent {
+  mascota: Mascotas = {
+    idmp :'',
+    raza:'',
+    tamano:'',
+    edad:0,
+    nombre:'',
+    sexo:'',
+    descripcion:'',
+    perdida:'',
+    imagenprincipal:'',
+    imagen2:'',
+    imagen3:'',
+    imagen4:'',
+    ciudad:'',
+    barrio:'',
+    fechaperdida:'',
+    nombredueno:'',
+    tel1:0,
+    tel2:0,
+    mail:'',
+  }
+  constructor(private route: ActivatedRoute, private mascotaService:ServicesService){}
 
+  ngOnInit():void{
+    const mascotaidmp = this.route.snapshot.paramMap.get('idmp')
+  }
 }
