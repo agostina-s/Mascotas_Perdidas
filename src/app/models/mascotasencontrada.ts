cd27fe33958e6667ddc4c5301da9144432405f15
SCanterA <salvador.cantera13@gmail.com>
2023-11-28 09:13:16 -0300

    comentando el codigo

--- src/app/models/mascotasencontrada.ts
+++ src/app/models/mascotasencontrada.ts
@@ -1,13 +1,14 @@
+// Definición de la interfaz Mascotasencontrada
 export interface Mascotasencontrada {
-    uid:string;
-    raza:string;
-    tamaño:string;
-    collar:string;
-    nombre:string;
-    sexo:string;
-    descripcion:string;
-    imagenprincipal:string;
-    imagenn2:string;
-    imagenn3:string;
-    imagenn4:string;
+    uid:string; // Identificador único de la mascota encontrada
+    raza:string; //raza mascota
+    tamaño:string; //tamaño mascota
+    collar:string; // Collar si/no
+    nombre:string;   // Nombre de la mascota
+    sexo:string;  // Sexo de la mascota
+    descripcion:string; // Descripción general de la mascota 
+    imagenprincipal:string; // Ruta o enlace a la imagen principal de la mascota
+    imagenn2:string;  // Ruta o enlace a la segunda imagen de la mascota
+    imagenn3:string;  // Ruta o enlace a la tercera imagen de la mascota 
+    imagenn4:string; // Ruta o enlace a la cuarta imagen de la mascota
 }
