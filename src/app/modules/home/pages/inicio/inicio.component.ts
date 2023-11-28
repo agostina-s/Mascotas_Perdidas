2d50750ce70258c9b789c02c0b90c549513d5ea3
SCanterA <salvador.cantera13@gmail.com>
2023-11-28 10:22:20 -0300

    comentando el codigo

--- src/app/modules/home/pages/inicio/inicio.component.ts
+++ src/app/modules/home/pages/inicio/inicio.component.ts
@@ -9,11 +9,11 @@ import * as $ from 'jquery';
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent {
-
+// Arreglo para almacenar las mascotas obtenidas del servicio CRUD
   coleccionMascotas: Mascotas[] = [];
-
+// Mascota seleccionada actualmente
   publiSeleccionada!: Mascotas;
-
+// Constructor para inyectar el servicio CRUD
   constructor(
     //declaramos el SERVICIO CRUD
     public servicioCRUD: ServicesService
@@ -28,11 +28,12 @@ export class InicioComponent {
     })
   }
 
+  // Método para obtener una descripción corta (primeros 10 palabras) de una cadena
   getDescripcionCorta(desc: string){
     return desc.split(' ').splice(0,10).join(' ') + '...';
   }
   
-
+ // Elementos del DOM
   flechaAtras: HTMLButtonElement = document.querySelector('#btnAtras') as HTMLButtonElement;
   cards: HTMLDivElement = document.querySelector('.slider-wrapper .card') as HTMLDivElement;
   cardsLista!: HTMLDivElement;
@@ -42,9 +43,11 @@ export class InicioComponent {
     this.cardsLista = document.querySelector('.slider-wrapper') as HTMLDivElement;
   }
 
+  // Método para desplazar a la derecha en el carrusel
   desplazarDerecha(){
     this.cardsLista.scrollLeft += 319;
   }
+   // Método para desplazar a la izquierda en el carrusel
   desplazarIzquierda(){
     this.cardsLista.scrollLeft -= 319;
   }
