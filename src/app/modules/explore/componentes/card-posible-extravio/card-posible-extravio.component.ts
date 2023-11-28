9ed9b1841d7b6742ffd4d2da003a8274d34e8f27
Gabituts <gabrielbastias25@gmail.com>
2023-11-28 19:05:59 -0300

    importaciones del componente

--- src/app/modules/explore/componentes/card-posible-extravio/card-posible-extravio.component.ts
+++ src/app/modules/explore/componentes/card-posible-extravio/card-posible-extravio.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Mascotasencontrada } from 'src/app/models/mascotasencontrada';
+//crud service
+import { ServicesService } from 'src/app/modules/admin/services/services.service';
+import 'bootstrap'
 
 @Component({
   selector: 'app-card-posible-extravio',
