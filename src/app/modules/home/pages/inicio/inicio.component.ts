b58dc14e9d6e086410a9f110b6012077ae93e345
Gabituts <gabrielbastias25@gmail.com>
2023-11-28 20:26:15 -0300

    carrousel copiado abajo funcionando y con toda la logica ademas de correcciones

--- src/app/modules/home/pages/inicio/inicio.component.ts
+++ src/app/modules/home/pages/inicio/inicio.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Mascotas } from 'src/app/models/mascotasperdidas';
+import { Mascotasencontrada } from 'src/app/models/mascotasencontrada';
 import { ServicesService } from 'src/app/modules/admin/services/services.service';
 import * as $ from 'jquery';
 
@@ -13,6 +14,13 @@ export class InicioComponent {
   coleccionMascotas: Mascotas[] = [];
 // Mascota seleccionada actualmente
   publiSeleccionada!: Mascotas;
+
+  // Arreglo para almacenar las mascotas encontradas obtenidas del servicio CRUD
+  coleccionMascotasEncontradas: Mascotasencontrada [] = [];
+  // Mascota seleccionada actualmente
+  publiSeleccionadaEncontrada!: Mascotasencontrada;
+
+
 // Constructor para inyectar el servicio CRUD
   constructor(
     //declaramos el SERVICIO CRUD
@@ -21,10 +29,17 @@ export class InicioComponent {
   ){}
 
   ngOnInit(): void{
-    /* del servicio Crud, llamamos a obtener producto y los guardamos
+
+    /* del servicio Crud, llamamos a obtener publicaciones y los guardamos
     en la colección */
     this.servicioCRUD.obtenerMascota().subscribe(mascota =>{
       this.coleccionMascotas = mascota;
+    })
+
+    /* del servicio Crud, llamamos a obtener publicaciones y los guardamos
+    en la colección */
+    this.servicioCRUD.obtenerMascotaEncontrada().subscribe(mascota =>{
+      this.coleccionMascotasEncontradas = mascota;
     })
   }
 
@@ -34,24 +49,34 @@ export class InicioComponent {
   }
   
  // Elementos del DOM
-  flechaAtras: HTMLButtonElement = document.querySelector('#btnAtras') as HTMLButtonElement;
-  cards: HTMLDivElement = document.querySelector('.slider-wrapper .card') as HTMLDivElement;
   cardsLista!: HTMLDivElement;
+
+   // Elementos del DOM2
+    cardsLista2!: HTMLDivElement;
 
   ngAfterViewInit() {
     // Aquí, la vista y sus elementos están completamente inicializados.
-    this.cardsLista = document.querySelector('.slider-wrapper') as HTMLDivElement;
+    this.cardsLista = document.querySelector('.slider1') as HTMLDivElement;
+    this.cardsLista2 = document.querySelector('.slider2') as HTMLDivElement;
   }
 
   // Método para desplazar a la derecha en el carrusel
-  desplazarDerecha(){
+  desplazarDerecha1(){
     this.cardsLista.scrollLeft += 319;
   }
    // Método para desplazar a la izquierda en el carrusel
-  desplazarIzquierda(){
+  desplazarIzquierda1(){
     this.cardsLista.scrollLeft -= 319;
   }
 
+    // Método para desplazar a la derecha en el carrusel
+    desplazarDerecha2(){
+      this.cardsLista2.scrollLeft += 319;
+    }
+     // Método para desplazar a la izquierda en el carrusel
+    desplazarIzquierda2(){
+      this.cardsLista2.scrollLeft -= 319;
+    }
 
   // CARROUSEL
   // tabs = document.querySelectorAll('.slider-wrapper card');
