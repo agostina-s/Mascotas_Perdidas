6ab7067490623e1f429d8c0c2c7991e7dae3d728
Gabituts <gabrielbastias25@gmail.com>
2023-11-28 19:24:22 -0300

    importaciones en publi posible extravio

--- src/app/modules/explore/pages/publicacion-posible-extravio/publicacion-posible-extravio.component.ts
+++ src/app/modules/explore/pages/publicacion-posible-extravio/publicacion-posible-extravio.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Mascotasencontrada } from 'src/app/models/mascotasencontrada';
+//CRUD Service
+import { ServicesService } from 'src/app/modules/admin/services/services.service';
+
 
 @Component({
   selector: 'app-publicacion-posible-extravio',
