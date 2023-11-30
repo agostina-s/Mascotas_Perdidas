72a63dd0970099cd112fe3077b29879c4937889e
Gabituts <gabrielbastias25@gmail.com>
2023-11-30 18:11:18 -0300

    ya se puede pedir el uid en el navbar

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -8,9 +8,23 @@ import { AuthService } from 'src/app/modules/auth/services/auth.service';
 })
 
 export class NavbarComponent implements OnInit{
+
+  userID!:string | undefined;
+
   constructor(
     public servicioAuth: AuthService, // servicioAuth referencia a nuestro servicio Auth
-  ){ }
+  ){ 
+    //LA GLORIA pide el estado de autentificacion en tiempo real y devuelve el userID
+    this.servicioAuth.authState().subscribe( res => {
+      if(res?.uid !== undefined){
+        this.userID = res?.uid
+        return this.userID
+      }else{
+        this.userID = undefined
+        return this.userID
+      }
+    })
+  }
   ngOnInit(): void {
     
   }
