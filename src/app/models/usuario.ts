4a4fc97803c808ed437521380d0362402a1001ac
SCanterA <salvador.cantera13@gmail.com>
2023-11-30 17:56:54 -0300

    comentando

--- src/app/models/usuario.ts
+++ src/app/models/usuario.ts
@@ -1,4 +1,4 @@
-//este es el interfaz para el inicio de sesion del usuario
+//declaracion de el interfaz para el inicio de sesion del usuario /propiedades 
 export interface Usuario {
     uid: string | any; // user id => id para auth de firebase
     name: string; 
