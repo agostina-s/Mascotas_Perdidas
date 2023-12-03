8af87a4454d0e82286048060831750bb29a07801
Gabituts <gabrielbastias25@gmail.com>
2023-12-03 01:13:23 -0300

    ahora si se muestran las publicaciones en mi perfil, solo requirio un cambio de toda la logica e importar bien el componente

--- src/app/modules/profile/pages/miperfil/miperfil.component.ts
+++ src/app/modules/profile/pages/miperfil/miperfil.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { catchError, combineLatest, of } from 'rxjs';
 import { Mascotas } from 'src/app/models/mascotasperdidas';
 import { Usuario } from 'src/app/models/usuario';
 import { ServicesService } from 'src/app/modules/admin/services/services.service';
 import { AuthService } from 'src/app/modules/auth/services/auth.service';
 import { FirestoreService } from 'src/app/shared/services/firestore.service';
-import { catchError, forkJoin } from 'rxjs';
+// import { catchError, forkJoin } from 'rxjs';
 
 
 @Component({
@@ -22,7 +23,8 @@ export class MiperfilComponent implements OnInit {
     private servicioAuth: AuthService, 
     private router: Router, 
     private servicioFirestore: FirestoreService,
-    private servicioCRUD: ServicesService){
+    private servicioCRUD: ServicesService,
+    private cdr: ChangeDetectorRef){
     //LA GLORIA pide el estado de autentificacion en tiempo real y devuelve el userID
     this.servicioAuth.authState().subscribe( res => {
       if(res?.uid !== undefined){
@@ -41,38 +43,53 @@ export class MiperfilComponent implements OnInit {
   ngOnInit() {
   }
 
-  obtenerMascotasUsuario(){
-    this.servicioCRUD.obtenerMascotaById(this.idsPublicaciones[0])
-      .subscribe(
-        (mascota: Mascotas) => {
-          console.log('Mascota obtenida:', mascota);
-        },
-        error => {
-          console.error('Error al obtener mascota:', error);
-        }
-      );
-  }
-
+  
   publicaciones: Mascotas[] = [];
   idsPublicaciones: string[] = [];
-  publicacionesActivas: Mascotas[] = [];
-  publicacion: Mascotas | undefined;
 
-  obtenerMascotasFor(){
-    for (const id of this.idsPublicaciones){
-      (this.servicioCRUD.obtenerMascotaById(id)).subscribe(
-        (mascota: Mascotas) => {
-          this.publicacion = mascota;
-          this.publicacionesActivas.push(this.publicacion)
-          console.log('Mascota obtenida:', this.publicacion);
-        },
-        error => {
-          console.error('Error al obtener mascota:', error);
-        }
-      );
-    }
-    console.log (this.publicacionesActivas)
+  obtenerMascotasUsuario(){
+    // Obtener las mascotas asociadas a las publicaciones del usuario
+    const observablesMascotas = this.usuario?.publicaciones.map((id) =>
+      this.servicioCRUD.obtenerMascotaById(id).pipe(
+        catchError((error) => {
+          console.error('Error al obtener mascota por ID:', error);
+          return of(null); // Retornar un observable de valor nulo
+        })
+      )
+    ) || [];
+
+    combineLatest(observablesMascotas).subscribe(
+      (mascotas: (Mascotas | null)[]) => {
+        // Filtrar las mascotas que no se pudieron obtener (valor nulo)
+        this.publicaciones = mascotas.filter((mascota) => mascota !== null) as Mascotas[];
+        console.log('Mascotas actualizadas:', this.publicaciones);
+      },
+      (error) => {
+        console.error('Error al obtener mascotas:', error);
+      }
+    );
   }
+
+
+  // publicacionesActivas: Mascotas[] = [];
+  // publicacion: Mascotas | undefined;
+
+  // obtenerMascotasFor(){
+  //   for (const id of this.idsPublicaciones){
+  //     (this.servicioCRUD.obtenerMascotaById(id)).subscribe(
+  //       (mascota: Mascotas) => {
+  //         this.publicacion = mascota as Mascotas;
+  //         this.publicacionesActivas.push(this.publicacion)
+  //         console.log('Mascota obtenida:', this.publicacion);
+  //         this.cdr.detectChanges();
+  //       },
+  //       error => {
+  //         console.error('Error al obtener mascota:', error);
+  //       }
+  //     );
+  //   }
+  //   console.log('Publicaciones Activas:', this.publicacionesActivas);
+  // }
 
 
   obtenerInfoUser(uid:string){
