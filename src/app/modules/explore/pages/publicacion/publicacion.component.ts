904b3820803b9ef1454f263f1d544da1951688a9
Gabituts <gabrielbastias25@gmail.com>
2023-12-03 11:44:54 -0300

    boton editar solo funciona si es dueño de la publicacion

--- src/app/modules/explore/pages/publicacion/publicacion.component.ts
+++ src/app/modules/explore/pages/publicacion/publicacion.component.ts
@@ -4,6 +4,7 @@ import { Mascotas } from 'src/app/models/mascotasperdidas';
 import { Router } from '@angular/router';
 //CRUD Service
 import { ServicesService } from 'src/app/modules/admin/services/services.service';
+import { AuthService } from 'src/app/modules/auth/services/auth.service';
 
 @Component({
   selector: 'app-publicacion',
@@ -14,9 +15,26 @@ export class PublicacionComponent{
   mascotas: Mascotas[] = []
 
   publicacion?:Mascotas;
+  userID!:string | undefined;
 
-  constructor(private acroutes:ActivatedRoute, public servicioCRUD:ServicesService, private router:Router){ //acroutes detecta id que esta en el url
 
+  constructor(private acroutes:ActivatedRoute,
+              public servicioCRUD:ServicesService, 
+              private router:Router,
+              private servicioAuth: AuthService){ //acroutes detecta id que esta en el url
+
+            //LA GLORIA pide el estado de autentificacion en tiempo real y devuelve el userID
+            this.servicioAuth.authState().subscribe( res => {
+              if(res?.uid !== undefined){
+                this.userID = res?.uid
+                console.log('la respuesta del observable:',this.userID)
+                return this.userID
+              }else{
+                this.userID = undefined
+                console.log('la respuesta del observable:',this.userID)
+                return this.userID
+              }
+            })
 
     //lo siguiente se ejecutara al momento de ser cargada la pagina
     //llama a la funcion obtenerMascotasById y envia el id para que le devuelva una publicacion especifica con subscribe()
