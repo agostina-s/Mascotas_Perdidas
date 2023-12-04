2e0e0d7597953706d470217700881e256f05c70b
Gabituts <gabrielbastias25@gmail.com>
2023-12-03 21:43:12 -0300

    corregido ingresar nombre en servicio

--- src/app/shared/services/firestorage.service.ts
+++ src/app/shared/services/firestorage.service.ts
@@ -9,9 +9,11 @@ export class FirestorageService {
 
   constructor( public fireStorage: AngularFireStorage) { }
 
-  subirImagen(file:any, path: string, nombre: string): Promise<string>{
+  subirImagen(file:any, path: string): Promise<string>{
     return new Promise( resolve =>{
 
+      const timestamp = new Date().getDate();
+      const nombre = `${timestamp}_${file.name}`
       const filePath = path + '/' + nombre;
       const ref = this.fireStorage.ref(filePath);
       const task = ref.put(file);
